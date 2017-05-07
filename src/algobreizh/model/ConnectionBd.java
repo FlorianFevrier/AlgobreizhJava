5d52a8dac2f6081571cd3c0a0aa9b1a73c74165b
florian <florian@192.168.1.13>
2017-05-07 19:17:56 +0200

    Rend le fichier de conf accessible depuis le jar.

--- src/algobreizh/model/ConnectionBd.java
+++ src/algobreizh/model/ConnectionBd.java
@@ -5,12 +5,13 @@
  */
 package algobreizh.model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -20,84 +21,71 @@ import java.util.Scanner;
  * @author florian
  */
 public class ConnectionBd {
-  /**
-   * Chemin de la base de données
-   */
-  private String url = "jdbc:mysql://51.254.222.13/algobreizh";
-  /**
-   * Fichier de conf de la base de données, contient l'identifiant et le mot de passe
-   */
-  private String conf = "src/algobreizh/model/config.txt";
-
-  /**
-   * Nom d'utilisateur 
-   */
-  private String user = null;
-  /**
-   * Mot de passe
-   */
-  private String passwd = null;
-
-  /**
-   * Une connexion à la base de données
-   */
-  private static Connection connect;
-
-  /**
-   * Constructeur privée ConnectionBd
-   * 
-   * <p>A la construction d'un objet ConnectionBd,
-   * on récupere les identifiants de connexion dans le fichier config.txt.
-   * Puis on se connecte à la base de données
-   * </p>
-   */
-  private ConnectionBd(){
-    //On va lire les info pour se connecter à la base de données dans un fichier texte
-    try
-    {
-        File f = new File (conf);
-        Scanner scanner = new Scanner (f);
-
-        while (true)
-        {
-            try
-            {
-                user = scanner.next();
-                passwd = scanner.next();
-            }
-            catch (NoSuchElementException exception)
-            {
-                break;
-            }
-        }
-
-        scanner.close();
-    }
-    catch (FileNotFoundException exception) {
-        System.out.println ("Le fichier n'a pas été trouvé");
-    }
+    /**
+     * Chemin de la base de données
+     */
+    private String url = "jdbc:mysql://51.254.222.13/algobreizh";
+  
+    /**
+     * Fichier de conf de la base de données, contient l'identifiant et le mot de passe
+    */
+    private InputStream in = getClass().getResourceAsStream("config.txt"); 
+  
+    /**
+     * Nom d'utilisateur 
+     */
+    private String user = null;
     
-    //On se connecte à la base de données
-    try { 
-        connect = DriverManager.getConnection(url, user, passwd);
+    /**
+     * Mot de passe
+     */
+    private String passwd = null;
 
-    } catch (SQLException e) {
-      e.printStackTrace();
+    /**
+     * Une connexion à la base de données
+     */
+    private static Connection connect;
+
+    /**
+     * Constructeur privée ConnectionBd
+     * 
+     * <p>A la construction d'un objet ConnectionBd,
+     * on récupere les identifiants de connexion dans le fichier config.txt.
+     * Puis on se connecte à la base de données
+     * </p>
+     */
+    private ConnectionBd(){
+      //On va lire les info pour se connecter à la base de données dans un fichier texte
+      try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            user = reader.readLine();
+            passwd = reader.readLine();
+      } 
+      catch (IOException e) {
+         System.err.println("Error: " + e);
+      }
+
+      //On se connecte à la base de données
+      try { 
+          connect = DriverManager.getConnection(url, user, passwd);
+
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
     }
 
-  }
+    /**
+     * Retourne une instance de la base de données
+     * @return une instance de la base de données, sous forme d'objet Connection
+     */
+     public static Connection getInstance(){
 
-  /**
-   * Retourne une instance de la base de données
-   * @return une instance de la base de données, sous forme d'objet Connection
-   */
-   public static Connection getInstance(){
+      if(connect == null){
+        new ConnectionBd();
+      }
 
-    if(connect == null){
-      new ConnectionBd();
-    }
+      return connect;   
 
-    return connect;   
-
-  }       
+    }       
 }
